12fc90a836d27eb0baab6eb088908133d16e6b8f
Kevin Dunne <kevinmdunne@gmail.com>
2014-12-08 20:56:18 +0000

    Update authentication service with new interface methods

--- services/user-authentication/src/main/java/com/sharegame/services/user/UserAuthenticationService.java
+++ services/user-authentication/src/main/java/com/sharegame/services/user/UserAuthenticationService.java
@@ -2,14 +2,20 @@ package com.sharegame.services.user;
 
 import com.mini.exception.InfastructureException;
 import com.mini.exception.ServiceExecutionException;
+import com.mini.io.adapter.IQueueAdapter;
 import com.mini.microservice.AbstractMicroservice;
+import com.mini.data.MicroserviceRequest;
 
 public class UserAuthenticationService extends AbstractMicroservice{
 
 	private static final String ID = "com.sharegame.services.user.UserAuthenticationService";
 	
+	public UserAuthenticationService(IQueueAdapter queueAdapter){
+		super(queueAdapter);
+	}
+	
 	@Override
-	public void execute() throws ServiceExecutionException {
+	public void execute(MicroserviceRequest request) throws ServiceExecutionException {
 
 	}
 
